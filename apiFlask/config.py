86d108f6cac39264e1d2437b0d233ee9cbf3ef55
Data the best <Data@TheBridge.local>
2021-06-03 13:09:47 +0200

    añado archivo config y requirements

---
+++ apiFlask/config.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+class Config:
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
